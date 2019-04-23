c673dbcc1d213a22183289147fd6d72682571334
Hazmi <whitespaces@gmail.com>
2019-04-23 08:20:15 +0700

    move the original css from npm package to ./src/index.css

--- src/components/todos/index.js
+++ src/components/todos/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Header } from './components/header';
 import { Main } from './components/main';
 import { Footer } from './components/footer';
-import 'todomvc-app-css/index.css';
 
 export const Todos = () => (
   <section className="todoapp">
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Todos from './components/todos';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(<Todos />, document.getElementById('root'));
 
