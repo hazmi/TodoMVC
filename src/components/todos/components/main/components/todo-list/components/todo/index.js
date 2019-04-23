a4384b6cb9877b2c1d9cb935f113249a76b089b2
Hazmi <whitespaces@gmail.com>
2019-04-23 20:04:47 +0700

    refactor todo item css

--- src/components/todos/components/main/components/todo-list/components/todo/index.js
+++ src/components/todos/components/main/components/todo-list/components/todo/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './index.scss';
 
 export const Todo = ({ checked, children }) => (
   <li className="todo">
-    <div className="view">
-      <input className="toggle" type="checkbox" checked={checked} />
-      <label>{children}</label>
-      <button className="destroy"/>
+    <div className="todo__view">
+      <input className="todo__toggle" type="checkbox" defaultChecked={checked} />
+      <label className="todo__label">{children}</label>
+      <button className="todo__destroy"/>
     </div>
+    <input type="text" className="todo__edit" />
   </li>
 );
 
