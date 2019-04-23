5c0f2bb9c191cc4c04a3de3ef13bca17e6d8c72e
Hazmi <whitespaces@gmail.com>
2019-04-23 21:31:47 +0700

    refactor todo and todolist css + add edit todo behavior

--- src/components/todos/components/main/components/todo-list/components/todo/index.js
+++ src/components/todos/components/main/components/todo-list/components/todo/index.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './index.scss';
 
-export const Todo = ({ checked, children }) => (
-  <li className="todo">
-    <div className="todo__view">
-      <input className="todo__toggle" type="checkbox" defaultChecked={checked} />
-      <label className="todo__label">{children}</label>
-      <button className="todo__destroy"/>
-    </div>
-    <input type="text" className="todo__edit" />
-  </li>
-);
+export const Todo = ({
+  completed,
+  children,
+  isLast
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const inputRef = useRef(null);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current.focus();
+    }, 200);
+  };
+
+  const handleSubmit = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <li
+      className={[
+        "todo",
+        isEditing ? "todo_editing" : null,
+        isLast ? "todo_last" : null,
+        completed ? "todo_completed" : null,
+      ].join(' ')}
+    >
+      <div className="todo__view">
+        <input className="todo__toggle" type="checkbox" defaultChecked={completed} />
+        <label
+          className="todo__label"
+          onDoubleClick={handleEdit}
+        >
+          {children}
+        </label>
+        <button className="todo__destroy"/>
+      </div>
+      <input
+        ref={inputRef}
+        type="text"
+        className="todo__edit"
+        onBlur={handleSubmit}
+      />
+    </li>
+  );
+};
 
 export default Todo;
--- src/components/todos/components/main/components/todo-list/index.js
+++ src/components/todos/components/main/components/todo-list/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Todo } from './components/todo';
+import './index.scss';
 
 export const TodoList = () => (
   <ul className="todo-list">
-    <Todo checked>Test 1</Todo>
+    <Todo completed>Test 1</Todo>
     <Todo>Test 2</Todo>
-    <Todo>Test 3</Todo>
+    <Todo isLast>Test 3</Todo>
   </ul>
 );
 
