b454c797ea0bf0c431c48bafce8be6108fd27009
Hazmi <whitespaces@gmail.com>
2019-04-23 18:54:17 +0700

    move footer from template to react component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Todos from './components/todos';
+import { Todos } from './components/todos';
+import { Footer } from './components/footer';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
-ReactDOM.render(<Todos />, document.getElementById('root'));
+const App = () => (
+  <div>
+    <Todos />
+    <Footer />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
