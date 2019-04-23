95ebeea5a150c44d9fc98703e2d6eadfd101fe9e
Hazmi <whitespaces@gmail.com>
2019-04-23 08:54:55 +0700

    refactor header css

--- src/components/todos/components/header/index.js
+++ src/components/todos/components/header/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './index.scss';
 
 export const Header = () => (
   <header className="header">
     <h1>todos</h1>
-    <input className="new-todo" placeholder="What needs to be done?" />
+    <input className="header__new-todo" placeholder="What needs to be done?" />
   </header>
 );
 
