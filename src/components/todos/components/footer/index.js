2c14dd72b70c8ceeeb2d90b95b382be9f0fc6aef
Hazmi <whitespaces@gmail.com>
2019-04-24 07:04:34 +0700

    count the actual data for footer

--- src/components/todos/components/footer/index.js
+++ src/components/todos/components/footer/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { TodoCount } from './components/todo-count';
 import { Filters } from './components/filters';
 
-export const Footer = () => {
+export const Footer = ({ data = [] }) => {
+  const count = data.reduce((total, item) => item.completed ? total : total + 1, 0);
+
   return (
     <footer className="footer">
-      <TodoCount count={3} />
+      <TodoCount count={count} />
       <Filters />
       <button className="clear-completed">Clear Completed</button>
     </footer>
